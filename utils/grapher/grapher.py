08d27b15b39466424a8fe00ab7ff954001702cdb
lanhamt <tlanham@cs.stanford.edu>
2017-06-18 07:11:58 +0000

    added files for grapher

---
+++ utils/grapher/grapher.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+SIM grapher CLI script to create aggregate simulation
+run graphs.
+
+Manages options for creating SIM graph using matplotlib.
+
+@author: Kate Stowell and Travis Lanham
+"""
+from sim_grapher import SimConfigGenerator
+
+import matplotlib
+matplotlib.use('Agg')
---
+++ utils/grapher/sim_grapher.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+SIM grapher class.
+
+Generates graph from multiple SIM runs, plotting
+load averge against latency.
+
+@author: Kate Stowell and Travis Lanham
+"""
+
+class SimGrapher:
+    def __init__(self):
+        pass
